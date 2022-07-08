93639497be8adadb4083c365487c55ba2c8bd09d
Alaa-Ezzat <alaa.awadallah97@gmail.com>
2022-07-08 14:04:21 +0200

    Updates

---
+++ precache-manifest.2bda56b3044a01bb0efb44a6928b3e3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d90075bf52f344665c",
+    "url": "/Moviely/static/css/main.426b5c3e.chunk.css"
+  },
+  {
+    "revision": "d5d90075bf52f344665c",
+    "url": "/Moviely/static/js/main.963b00c8.chunk.js"
+  },
+  {
+    "revision": "c6fe4f3c778991f488a6",
+    "url": "/Moviely/static/js/runtime~main.3b01a78f.js"
+  },
+  {
+    "revision": "2fd1440c77cb07d09c5a",
+    "url": "/Moviely/static/css/2.736847a5.chunk.css"
+  },
+  {
+    "revision": "2fd1440c77cb07d09c5a",
+    "url": "/Moviely/static/js/2.05278725.chunk.js"
+  },
+  {
+    "revision": "b3ad1b1743e1d8b39b06b342c6b142db",
+    "url": "/Moviely/static/media/banner2.b3ad1b17.jpg"
+  },
+  {
+    "revision": "c3ef1e44c1c6807428f8f492af505c6d",
+    "url": "/Moviely/static/media/avatar.c3ef1e44.jpg"
+  },
+  {
+    "revision": "5010ab1d7a2c3cbc92942661cb3b68b9",
+    "url": "/Moviely/static/media/elsisy.5010ab1d.jpg"
+  },
+  {
+    "revision": "d061bcec4d95b53fdbb9d61729f021f6",
+    "url": "/Moviely/static/media/footer.d061bcec.jpg"
+  },
+  {
+    "revision": "86a1c1f86f80fc3757abbca2c95e4163",
+    "url": "/Moviely/index.html"
+  }
+];
